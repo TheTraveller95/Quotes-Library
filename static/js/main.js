401f81020bfdf62c4f85e808c194639280861f79
TheTraveller95 <crivellaroandres@gmail.com>
2020-02-03 21:40:55 +0000

    fix the modify_quote function

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,7 @@
 $('#cat-button-mobile').click(function(){
     document.getElementById("mobile-demo").style.display="none";
 });
+
+$('#submit').click(function(){
+    document.getElementById('category_name').toUppercase();
+})
